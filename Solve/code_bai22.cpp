7fc617c3da06d147eaa31519c20dfe7c23edb22c
NTTVy03 <trucvy05062003@gmail.com>
2024-01-02 20:40:37 +0700

    Bai 22 - tim Cau

--- Solve/code_bai22.cpp
+++ Solve/code_bai22.cpp
@@ -11,19 +11,27 @@ using namespace std;
 
 const int MAX = 100005;
 int n, m;
-vector<int> adj[MAX];
-int low[MAX], num[MAX], parent[MAX];
+vector<pair<int,int>> adj[MAX];
+int low[MAX], num[MAX], parent[MAX], numchild[MAX];
+int res[MAX];
 int cnt = 0;
 
 void dfs(int u) {
     low[u] = num[u] = ++cnt;
-    for (int v : adj[u]) {
+    numchild[u] = 1;
+
+    for (pair<int,int> p : adj[u]) {
+        int v = p.first;
+        int id = p.second;
+
         if (num[v] == 0) {
             parent[v] = u;
             dfs(v);
             low[u] = min(low[u], low[v]);
+            numchild[u] += numchild[v];
             if (low[v] > num[u]) {
-                cout << u << " " << v << endl;
+                // u - v is a bridge
+                res[id] = numchild[v] * (n - numchild[v]);
             }
         } else if (v != parent[u]) {
             low[u] = min(low[u], num[v]);
@@ -40,12 +48,27 @@ int main() {
     inp >> n >> m;
     for (int i = 1; i <= m; i++) {
         int u, v; inp >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+        adj[u].push_back(make_pair(v, i));
+        adj[v].push_back(make_pair(u, i));
     }
 
     inp.close();
-    
+
     dfs(1);
+
+    ofstream out(fileout);
+
+    for (int i = 1; i <= m; i++) {
+        out << res[i] << endl;
+    }
+
+    out.close();
+
     return 0;
 }
+
+/*
+Độ phức tạp:
+- Thời gian: O(n + m)
+- Bộ nhớ: O(n + m)
+*/
