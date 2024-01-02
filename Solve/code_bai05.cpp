7e6c465c1b95f650242fb38c1186dff20efcc45a
NTTVy03 <trucvy05062003@gmail.com>
2024-01-02 12:36:05 +0700

    Bai 05 - Hash

--- Solve/code_bai05.cpp
+++ Solve/code_bai05.cpp
@@ -1,3 +1,93 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+#define BASE 256
+#define MOD (int)(1e9 + 7)
+
+using namespace std;
+
+void readData(string filein, string &para, string &word) {
+    ifstream inp(filein.c_str());
+
+    getline(inp, para);
+    getline(inp, word);
+
+    inp.close();
+}
+
+void solve(string para, string word, vector<int> &ans) {
+    // find positions of word in para
+
+    // calculate POW
+    vector<long long> POW(para.size());
+    POW[0] = 1;
+    for (int i = 1; i < para.size(); i++)
+        POW[i] = POW[i-1] * BASE % MOD;
+
+    // hash of word
+    long long hashWord = 0;
+    for (int i = 0; i < word.size(); i++)
+    {
+        hashWord = ((hashWord * BASE)  % MOD + word[i]) % MOD;
+    }
+    
+
+    // hash of para
+    vector<long long> hashPara(para.size());
+    hashPara[0] = para[0];
+    for (int i = 1; i < para.size(); i++)
+    {   
+        hashPara[i] = ((hashPara[i-1] * BASE) % MOD + para[i]) % MOD;
+    }
+
+    // find positions
+    for (int i = 0; i <= para.size() - word.size(); i++) {
+        long long hashSub = hashPara[i + word.size() - 1];
+        if (i > 0)
+            hashSub = (hashSub - ((hashPara[i-1] * POW[word.size()]) % MOD) + MOD) % MOD;
+
+        if (hashSub == hashWord) {
+            bool ok = true;
+            for (int j = 0; j < word.size(); j++)
+                if (para[i+j] != word[j]) {
+                    ok = false;
+                    break;
+                }
+            
+            if (ok) {
+                // ans with id from 1
+                ans.push_back(i + 1);
+            }
+        }
+    }
+
+}
+
+void printAns(string fileout, vector<int> &ans) {
+    ofstream out(fileout.c_str());
+
+    for (int i = 0; i < ans.size(); i++)
+        out << ans[i] << " ";
+
+    out.close();
+}
+
+int main() {
+    string filein = "bai05.inp";
+    string fileout = "bai05.out";
+
+    string para, word;
+
+    readData(filein, para, word);
+    vector<int> ans;
+    solve(para, word, ans);
+    printAns(fileout, ans);
+
+    return 0;
+}
+
 /*
 Ý TƯỞNG HASH:
 
